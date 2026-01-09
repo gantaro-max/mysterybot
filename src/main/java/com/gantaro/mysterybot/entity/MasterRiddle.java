8b2e574df6cddff16b3e7b317f7f63f986d7c325
gantaro <gantaro1967@gmail.com>
2026-01-09 17:11:13 +0900

    ヒント_画像_Product画面追加

--- src/main/java/com/gantaro/mysterybot/entity/MasterRiddle.java
+++ src/main/java/com/gantaro/mysterybot/entity/MasterRiddle.java
@@ -3,14 +3,14 @@ package com.gantaro.mysterybot.entity;
 import lombok.Data;
 
 @Data
-public class Riddle {
+public class MasterRiddle {
+
     private Integer id;
-    private String groupId;
-    private Integer stageNo;
     private String question;
     private String answer;
+    private String hintMsg;
     private String nextMsg;
     private Integer imageId;
-    private String hintMsg;
+    private String category;
 
 }
---
+++ src/main/java/com/gantaro/mysterybot/repository/MasterRiddleRepository.java
@@ -0,0 +1,18 @@
+package com.gantaro.mysterybot.repository;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import com.gantaro.mysterybot.entity.MasterRiddle;
+
+@Mapper
+public interface MasterRiddleRepository {
+
+    List<MasterRiddle> findAll();
+
+    MasterRiddle findById(Integer id);
+
+    void insert(MasterRiddle masterRiddle);
+
+    void delete(Integer id);
+
+}
